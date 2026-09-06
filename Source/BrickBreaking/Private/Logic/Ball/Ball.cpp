21b93ff517d08c9889e4e4277f1e13d5aa8e9c20
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-09-05 20:19:56 -0400

    Start ball

---
+++ Source/BrickBreaking/Private/Logic/Ball/Ball.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Logic/Ball/Ball.h"
+
+#include "Components/SphereComponent.h"
+
+
+// Sets default values
+ABall::ABall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	RootComponent = SphereComponent;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(SphereComponent);
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/BrickBreaking/Public/Logic/Ball/Ball.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Ball.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class BRICKBREAKING_API ABall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
