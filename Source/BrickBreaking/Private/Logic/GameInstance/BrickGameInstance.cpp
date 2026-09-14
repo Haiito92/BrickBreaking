48e0b45daf113d5ebfcd6382400b85172612b1fc
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-09-14 12:24:12 -0400

    Fix Navigation keys and moving keys

--- Source/BrickBreaking/Private/Logic/GameInstance/BrickGameInstance.cpp
+++ Source/BrickBreaking/Private/Logic/GameInstance/BrickGameInstance.cpp
@@ -3,6 +3,7 @@
 
 #include "BrickBreaking/Public/Logic/GameInstance/BrickGameInstance.h"
 
+#include "Framework/Application/NavigationConfig.h"
 #include "Logic/Audio/AudioGameInstanceSubsystem.h"
 
 void UBrickGameInstance::Init()
@@ -12,4 +13,14 @@ void UBrickGameInstance::Init()
 	UAudioGameInstanceSubsystem* AudioSystem = GetSubsystem<UAudioGameInstanceSubsystem>();
 	
 	if (IsValid(AudioSystem)) AudioSystem->InitializeAudioSystem();
+	
+	
+	TSharedPtr<FNavigationConfig> NavConfig = FSlateApplication::Get().GetNavigationConfig();
+	if (NavConfig.IsValid())
+	{
+		NavConfig->KeyEventRules.Add(EKeys::W, EUINavigation::Up);
+		NavConfig->KeyEventRules.Add(EKeys::S, EUINavigation::Down);
+		NavConfig->KeyEventRules.Add(EKeys::A, EUINavigation::Left);
+		NavConfig->KeyEventRules.Add(EKeys::D, EUINavigation::Right);
+	}
 }
